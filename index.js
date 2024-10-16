a8a052daf8fc0a76772b07ecd80415cc37f6d405
krunalPatel4426 <YOUR_EMAIL>
2024-10-16 18:04:51 +0530

    try

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.use(
       secure: true, //Only have true when deploying on https
       sameSite: "None",
       rolling: true,
-      httpOnly: true,
+      // httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
