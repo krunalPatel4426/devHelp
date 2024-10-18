adf08223ef4e9b9fae50f0f0faefea78960ae794
krunalPatel4426 <YOUR_EMAIL>
2024-10-19 03:31:57 +0530

    lang chnage

--- src/controllers/programmingLanguage/programmingLanguage.controller.js
+++ src/controllers/programmingLanguage/programmingLanguage.controller.js
@@ -1,26 +1,45 @@
+import { Course } from "../../models/course.model.js";
 import { ProgrammingLanguage } from "../../models/programmingLanguage.model.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
 const getDataOfLang = asyncHandler(async (req, res) => {
-    try{
-        const programmingLanguageData = await ProgrammingLanguage.find({});
-        let data = []
-        programmingLanguageData.forEach((each) => {
-            data.push({
-                id: each._id,
-                title: each.name,
-                description: each.description,
-                img: each.img,
-                courses: each.courses
-            });
-        });
-        res.status(200).json({
-            "message" : "data fetched successfully",
-            data: data
-        });
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-})
+  try {
+    const programmingLanguageData = await ProgrammingLanguage.find({});
+    let data = [];
+    programmingLanguageData.forEach((each) => {
+      data.push({
+        id: each._id,
+        title: each.name,
+        description: each.description,
+        img: each.img,
+        courses: each.courses,
+      });
+    });
+    res.status(200).json({
+      message: "data fetched successfully",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-export { getDataOfLang };
+const getDataById = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  try {
+    const lang = await ProgrammingLanguage.findById(id).select("-__v");
+    if (!lang) return res.status(400).json({ message: "data not found" });
+    const courses = await Course.find(
+      { _id: { $in: lang.courses } },
+      "-averageRating -videoLink -__v -reviews"
+    );
+    res.status(200).json({ message: "data fetch successfully", langauage: lang, courses: courses });
+  } catch (error) {
+    return res.status(500).json({
+      message: "error while retriving data",
+    });
+  }
+});
+
+export { getDataById, getDataOfLang };
+
--- src/routes/programmingLanguageRoute.js
+++ src/routes/programmingLanguageRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getDataOfLang } from "../controllers/programmingLanguage/programmingLanguage.controller.js";
+import { getDataById, getDataOfLang } from "../controllers/programmingLanguage/programmingLanguage.controller.js";
 
 const router = Router();
 
 router.route("/getData").get(getDataOfLang);
+router.route("/getData/:id").get(getDataById);
 
 export default router;
