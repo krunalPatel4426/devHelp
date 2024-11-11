f02bdf51b7e6cd1c24552d730086c07ea0920cea
krunalPatel4426 <krunalp.official@gmail.com>
2024-11-11 20:13:39 +0530

    cors

--- src/controllers/github oauth2/github.controller.js
+++ src/controllers/github oauth2/github.controller.js
@@ -30,7 +30,8 @@ const githubLogin = asyncHandler(async (req, res) => {
   console.log(accessToken, refreshToken)
   res.cookie("accessToken", accessToken, options);
   res.cookie("refreshToken", refreshToken, options);
-  return res.redirect("https://github-o-auth-2-test.vercel.app/profile");
+  return res.redirect("https://devhelpp.onvercel.com/")
+  // return res.redirect("https://github-o-auth-2-test.vercel.app/profile");
   // return res.redirect("http://localhost:5173/profile")
   // return res.status(200).json({ message: "data got", user: req.user });
 });
